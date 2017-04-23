57d4e3985d9c2745d9e3d05a25a0f094c63f1a7f
arcward <git@edawrd.sh>
2017-04-22 20:37:59 -0400

    Updated to reflect rename

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 ==============
-echopy-example
+echokit-example
 ==============
-A sample Alexa skill using echopy_ (https://github.com/arcward/echopy)
+A sample Alexa skill using echokit_ (https://github.com/arcward/echokit)
 
 ==========
 Setting up
@@ -22,12 +22,12 @@ Alexa
 See the `official docs`_ for a more comprehensive guide.
 
 After setting your skill name/invocation name, copy the application ID. Then,
-open ``echopy-example/order_skill/order_skill.py`` and set
-``echopy.application_id`` as your own app ID.
+open ``echokit-example/order_skill/order_skill.py`` and set
+``echokit.application_id`` as your own app ID.
 
 Interaction model
 ^^^^^^^^^^^^^^^^^
-See ``echopy-example/interaction_model/``
+See ``echokit-example/interaction_model/``
 
 **Intent schema**::
 
@@ -64,18 +64,18 @@ Add a custom slot type ``MENU_ITEM``, with whatever values you'd like.
 
 Creating the Lambda deployment ZIP
 ----------------------------------
-After installing echopy_, in the command line, from
-the ``echopy-example/`` directory, run ``echodist --dir order_skill``. For
+After installing echokit_, in the command line, from
+the ``echokit-example/`` directory, run ``echodist --dir order_skill``. For
 example, if you cloned this repo in your home directory:
 
 .. code-block:: bash
 
-    ~/echopy-example $ echodist --dir order_skill/
+    ~/echokit-example $ echodist --dir order_skill/
 
 You can then upload the resulting ``order_skill.zip`` file via the Lambda
 console and test your skill from there or the `Alexa dev portal`_!
 
-.. _echopy: https://github.com/arcward/echopy
+.. _echokit: https://github.com/arcward/echokit
 .. _`official docs`: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/developing-an-alexa-skill-as-a-lambda-function#creating-a-lambda-function-for-an-alexa-skill
 
 .. _`Alexa dev portal`: https://developer.amazon.com/edw/home.html#/
--- order_skill/order_skill.py
+++ order_skill/order_skill.py
@@ -1,36 +1,36 @@
-import echopy
-from echopy import Response, PlainTextOutputSpeech, SimpleCard
+import echokit
+from echokit import Response, PlainTextOutputSpeech, SimpleCard
 
 # In the Lambda config, 'handler' would be
 # set to ``order_skill.handler``
-handler = echopy.handler
+handler = echokit.handler
 
 # Your skill ID, as provided in the Alexa dev portal
-echopy.application_id = "amzn1.ask.skill.a22e585b-20fe-420c-bba2-bf3a34e7691a"
+echokit.application_id = "amzn1.ask.skill.a22e585b-20fe-420c-bba2-bf3a34e7691a"
 
 
 # All apps are required to handle three basic requests,
 # which have their own decorators:
-# * LaunchRequest:          @echopy.on_session_launch
-# * SessionEndedRequest:    @echopy.on_session_end
-# * IntentRequest:          @echopy.on_intent('your_intent_name')
+# * LaunchRequest:          @echokit.on_session_launch
+# * SessionEndedRequest:    @echokit.on_session_end
+# * IntentRequest:          @echokit.on_intent('your_intent_name')
 
 # Handles: LaunchRequest
-@echopy.on_session_launch
+@echokit.on_session_launch
 def session_started(request, session):
     output_speech = PlainTextOutputSpeech("Welcome to Order Maker!")
     return Response(output_speech=output_speech)
 
 
 # Handles: SessionEndedRequest
-@echopy.on_session_end
+@echokit.on_session_ended
 def session_ended(request, session):
     output_speech = PlainTextOutputSpeech("See you later")
     return Response(output_speech=output_speech)
 
 
 # Handles: IntentRequest
-@echopy.on_intent('HoursIntent')
+@echokit.on_intent('HoursIntent')
 def hours_intent(request, session):
     output_speech = PlainTextOutputSpeech("We're open today from 5am to 8pm")
     return Response(output_speech=output_speech)
@@ -41,7 +41,7 @@ def hours_intent(request, session):
 # showing how to access the intent's 'Order' slot.
 # This would return output speech like: 'You asked me to jump'
 # The session variable would be returned on the next invocation
-@echopy.on_intent('OrderIntent')
+@echokit.on_intent('OrderIntent')
 def order_intent(request, session):
     menu_item = request.intent.slots['MenuItem'].value
     response_text = f'You ordered: {menu_item}'
@@ -54,11 +54,11 @@ def order_intent(request, session):
 # Handles: IntentRequest (unimplemented intent)
 # For example, if 'WeaveBasketIntent' is defined in your
 # interaction model, but you haven't defined a handler
-# for it with @echopy.on_intent('WeaveBasketIntent'),
+# for it with @echokit.on_intent('WeaveBasketIntent'),
 # this will catch it. If you don't define your own here,
-# by default echopy will return a "Sorry, I didn't
+# by default echokit will return a "Sorry, I didn't
 # understand your request" speech response.
-@echopy.fallback
+@echokit.fallback
 def unimplemented(request, session):
     output_speech = PlainTextOutputSpeech(f"Sorry, {request.intent.name} "
                                           f"isn't implemented!")
